703769c72a0783c23cd4755ac2a96f5059a9af4a
Hervé de CHAVIGNY <vevedh@gmail.com>
2023-05-02 16:43:53 -0400

    Première version

---
+++ lib/Service/NoteNotFound.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Hervé de CHAVIGNY <vevedh@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\NewsLetterBuilder\Service;
+
+class NoteNotFound extends \Exception {
+}
